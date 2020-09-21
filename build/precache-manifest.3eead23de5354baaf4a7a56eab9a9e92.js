d85947cc6696a43a79b74fcce5f287d2e90f5a10
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-09-21 23:19:53 +0300

    Changes

---
+++ build/precache-manifest.3eead23de5354baaf4a7a56eab9a9e92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f798b860f4d9d3f2bcfa7c5c63afbd4d",
+    "url": "/ReactJS-StyledComponents/index.html"
+  },
+  {
+    "revision": "cf383312fda2d2fb174a",
+    "url": "/ReactJS-StyledComponents/static/css/main.13bca9cb.chunk.css"
+  },
+  {
+    "revision": "dfeb5d33bc1a6bdbc2fc",
+    "url": "/ReactJS-StyledComponents/static/js/2.a8ef1270.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS-StyledComponents/static/js/2.a8ef1270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf383312fda2d2fb174a",
+    "url": "/ReactJS-StyledComponents/static/js/main.9bc4255b.chunk.js"
+  },
+  {
+    "revision": "4d71d5e45f06717f4a30",
+    "url": "/ReactJS-StyledComponents/static/js/runtime-main.d8a419d5.js"
+  },
+  {
+    "revision": "d964e0b3c644193e5591758f01e91f68",
+    "url": "/ReactJS-StyledComponents/static/media/padlock.d964e0b3.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 
 import SignIn from "./Form/Login/Login.js";
 import SignUp from "./Form/Register/Register.js";
+
+export const MENU = [{
+    title: "Login",
+    path: "/login",
+    component: SignIn,
+    exact: true
+}, {
+    title: "Register",
+    path: "/register",
+    component: SignUp,
+    exact: true
+}
+];
 
 class App extends Component {
     render(){
         return (
             <Router>
                 <div>
-                    <Route exact path="/" component={SignIn} />
-                    <Route exact path="/login" component={SignIn} />
-                    <Route exact path="/register" component={SignUp} />
+                    <Route path="/" exact render={() => <Redirect to="/login"/>} />
+                    {MENU.map(({path, component: Component, exact = false}) => (
+                        <Route  path={path}
+                                exact={exact}
+                                component={Component}/>
+                    ))}
                 </div>
             </Router>
         );
